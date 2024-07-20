c9f36566cb8def95140f38e8332a4431686355a8
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2024-07-19 20:38:18 -0500

    Fixed dark gui.

--- src/main/java/com/flanks255/simplybackpacks/gui/SBGui.java
+++ src/main/java/com/flanks255/simplybackpacks/gui/SBGui.java
@@ -55,7 +55,6 @@ public class SBGui extends AbstractContainerScreen<SBContainer> {
 
     @Override
     public void render(@Nonnull GuiGraphics gg, int pMouseX, int pMouseY, float pPartialTicks) {
-        this.renderBackground(gg, pMouseX, pMouseY, pPartialTicks);
         super.render(gg,pMouseX, pMouseY, pPartialTicks);
         this.renderTooltip(gg, pMouseX, pMouseY);
     }
